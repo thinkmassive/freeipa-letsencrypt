4bb5720aa6374b070ddbb9d28466df36e8563567
thinkmassive <alex@thinkmassive.org>
2021-01-19 18:16:17 -0500

    update root & intermediate certs used during install

--- setup-le.sh
+++ setup-le.sh
@@ -5,10 +5,10 @@ WORKDIR=$(dirname "$(realpath $0)")
 
 dnf install letsencrypt -y
 
-ipa-cacert-manage install "$WORKDIR/ca/DSTRootCAX3.pem" -n DSTRootCAX3 -t C,,
+ipa-cacert-manage install "$WORKDIR/ca/isrgrootx1.pem" -n ISRGRootX1 -t C,,
 ipa-certupdate -v
 
-ipa-cacert-manage install "$WORKDIR/ca/LetsEncryptAuthorityX3.pem" -n letsencryptx3 -t C,,
+ipa-cacert-manage install "$WORKDIR/ca/lets-encrypt-r3.pem" -n letsencryptr3 -t C,,
 ipa-certupdate -v
 
 "$WORKDIR/renew-le.sh" --first-time
